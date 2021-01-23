52b75cb079263a194272824a34f299806b3022cf
BlackTiger96 <radhweneamri9631@gmail.com>
2021-01-23 22:48:55 +0100

    main file updated

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from sys import exit
+import time
+import datetime
+from colorama import init, Fore, Style
 from Patient import Patient
 from Rendez_vous import Rendez_vous
 
@@ -10,32 +13,71 @@ main_msg = '''
     3- Supprimer un Patient
     4- Ajouter un Rendez-vous
     5- Annuler un Rendez-vous
-    6- Afficher la liste des Patients
-    7- Afficher la liste des Rendez-Vous
-    8- Vider la liste des patients
-    9- Vider la liste des rendez-vous
-    10- Revenu Total
-    11- Reafficher la liste des choix
-    12- Quitter
+    6- Verifier un Rendez-vous
+    7- Afficher la liste des Patients
+    8- Afficher la liste des Rendez-Vous
+    9- Vider la liste des patients
+    10- Vider la liste des rendez-vous
+    11- Revenu Total
+    12- Reafficher la liste des choix
+    13- Quitter
 '''
 
-def get_choice():
-    choice = input("CHOIX: ")
-    while choice not in str(range(1, 13)):
-        print("LE CHOIX DOIT ETRES ENTRE 1 ET 12 DE LA LISTE CI DESSUS !")
-        choice = input("CHOIX: ")
-    return int(choice)
+#time tuple
 
+#0--> year (4 digit)
+#1--> month (1-12)
+#2--> day (1-31)
+
+#3--> hour (0-23)
+#4--> minute (0-59)
+#5--> second (0-61)
+
+
+today = str(time.localtime()[0])+"-0"+str(time.localtime()[1])+"-"+str(time.localtime()[2])
+now = str(time.localtime()[3])+":"+str(time.localtime()[4])+":"+str(time.localtime()[5])
+
+#check rdv time
+#0--> Now
+#1--> 1 attendez and return time
+#2--> 2 "retard" and return "retard" duration
+
+def compare_time(t_now, t_rdv):
+    rdv_timestamp = datetime.datetime.strptime(t_rdv[0]+" "+t_rdv[1], "%Y-%m-%d %H:%M:%S").timestamp()
+    now_timestamp = datetime.datetime.strptime(t_now[0]+" "+t_now[1], "%Y-%m-%d %H:%M:%S").timestamp()
+    if rdv_timestamp < now_timestamp:
+        return True
+    else:
+        return False
+
+def pred(text):
+    init(convert=True)
+    print(Fore.RED + text, end="") 
+    print(Style.RESET_ALL)
+
+def pgreen(text):
+    init(convert=True)
+    print(Fore.GREEN + text, end="") 
+    print(Style.RESET_ALL)
 
 
 if __name__ == "__main__":
     print("============================================================================")
     print("______------*********======== Cabinet Dentaire ========*********------______")
-    print("============================================================================")
-    print("Selectionner une choix: ")
+    print("============================================================================\n")
+    
+    print(" --------------------------")
+    print("| AUJOURD'HUI: ", today, "|")
+    print(" --------------------------")
+
+    print(" --------------------------")
+    print("| MAINTENANT: ", now,"   |")
+    print(" --------------------------\n")
+
+    print("====>Selectionner un choix: ")
     print(main_msg)
 
-    choice = get_choice()
+    choice = int(input("CHOIX: "))
     while choice:
         # patients operations
         if choice == 1:
@@ -47,7 +89,8 @@ if __name__ == "__main__":
             prenom = input("PRENOM: ")
             date_naiss = input("DATE DE NAISSANCE(JJ-MM-AAAA): ")
             num_tel = input("NUMERO DE TELEPHONE: ")
-            if Patient.__add__(Patient(cin, nom, prenom, date_naiss, num_tel)):
+            n_pat = Patient(cin, nom, prenom, date_naiss, num_tel)
+            if Patient.__add__(n_pat):
                 print("PATIENT AJOUTE :)\n")
             else:
                 print("ERREUR: VERIFIER LES INFORMATION !\n")
@@ -74,7 +117,7 @@ if __name__ == "__main__":
                 print("PATIENT SUPPRIME\n")
             else:
                 print("ERREUR/ VERIFIER LES INFORMATIONS !\n")
-        elif choice == 6:
+        elif choice == 7:
             print("\n==============================")
             print("LISTE DES PATIENTS: ")
             print("==============================")
@@ -113,7 +156,69 @@ if __name__ == "__main__":
                 print("RENDEZ-VOUS ANNULE :)\n")
             else:
                 print("ERREUR: VERIFIER LES INFORMATION !")
-        elif choice == 7:
+        elif choice == 6:
+            print("\n==============================")
+            print("VERIFIER UN RENDEZ-VOUS: ")
+            print("==============================")
+            print("1 ---->AUJOURD'HUI")
+            print("2 ---->TOUT")
+            print("3 ---->DERNIER")
+            _choice = int(input("CHOIX ---->: "))
+            cin = int(input("CIN: "))
+            if _choice == 1:
+                today_rdv = Rendez_vous.__getallbydate__(Rendez_vous, cin, today)
+                if today_rdv:
+                    print("******************************")
+                    print("RENDEZ-VOUS: ")
+                    print("******************************")
+                    for i in today_rdv:
+                        if compare_time([i.date, i.heure], [str(today), str(now)]):
+                            pgreen("DATE: "+ i.date)
+                            pgreen("HEURE: "+ i.heure)
+                            print("------------------------------")
+                        else:
+                            pred("DATE: "+ i.date)
+                            pred("HEURE: "+ i.heure)
+                            print("------------------------------")
+                else:
+                    pred("PATIENT N'EXISTE PAS OU VOUS N'AVEZ PAS UN RENDEZ-VOUS !\n")
+            elif _choice == 2:
+                all_rdvs = Rendez_vous.__getallbycin__(Rendez_vous, cin)
+                if all_rdvs:
+                    print("******************************")
+                    print("RENDEZ-VOUS: ")
+                    print("******************************")
+                    for i in all_rdvs:
+                        if compare_time([i.date, i.heure], [str(today), str(now)]):
+                            pgreen("DATE: "+ i.date)
+                            pgreen("HEURE: "+ i.heure)
+                            print("------------------------------")
+                        else:
+                            pred("DATE: "+ i.date)
+                            pred("HEURE: "+ i.heure)
+                            print("------------------------------")
+                else:
+                    print("PATIENT N'EXISTE PAS OU VOUS N'AVEZ PAS UN RENDEZ-VOUS !\n")
+            elif _choice == 3:
+                last_rdv = Rendez_vous.__get__(Rendez_vous, cin)
+                if last_rdv:
+                    if compare_time([last_rdv.date, last_rdv.heure], [str(today), str(now)]):
+                        pgreen("******************************")
+                        pgreen("RENDEZ-VOUS: ")
+                        pgreen("******************************")
+                        pgreen("DATE: "+last_rdv.date)
+                        pgreen("HEURE: "+last_rdv.heure)
+                        pgreen("------------------------------\n")
+                    else:
+                        pred("******************************")
+                        pred("RENDEZ-VOUS: ")
+                        pred("******************************")
+                        pred("DATE: "+last_rdv.date)
+                        pred("HEURE: "+last_rdv.heure)
+                        pred("------------------------------\n")
+                else:
+                    print("PATIENT N'EXISTE PAS OU VOUS N'AVEZ PAS UN RENDEZ-VOUS !\n")
+        elif choice == 8:
             print("\n==============================")
             print("LISTE DES RENDEZ-VOUS: ")
             print("==============================")
@@ -135,14 +240,20 @@ if __name__ == "__main__":
                     print("******************************")
                     print("INFORMATIONS DU RENDEZ-VOUS")
                     print("******************************")
-                    print("DATE: ",i.date)
-                    print("HEURE: ", i.heure)
+                    if compare_time([i.date, i.heure], [str(today), str(now)]):
+                        pgreen("DATE: "+i.date)
+                        pgreen("HEURE: "+ i.heure)
+                        print("")
+                    else:
+                        pred("DATE: "+i.date)
+                        pred("HEURE: "+ i.heure)
+                        print("")
                     counterr += 1
             else:
                 print("LISTE VIDE !\n")
         #vider la liste des patients
         #!!!!!!!!!!!!!!!automatiquement les autres tables seront vides aussi
-        elif choice == 8:
+        elif choice == 9:
             print("\n==============================")
             print("VIDER LA LISTE DES PATIENTS")
             print("==============================")
@@ -150,7 +261,7 @@ if __name__ == "__main__":
                 print("LISTE DES PATIENTS MAINTENANT VIDE :)\n")
             else:
                 print("ERREUR !")
-        elif choice == 9:
+        elif choice == 10:
             print("\n==============================")
             print("VIDER LA LISTE DES RENDEZ-VOUS")
             print("==============================")
@@ -158,14 +269,14 @@ if __name__ == "__main__":
                 print("LISTE DES PATIENTS MAINTENANT VIDE :)\n")
             else:
                 print("ERRUER !")
-        elif choice == 10:
+        elif choice == 11:
             print("\n==============================")
             print("REVENU TOTAL")
             print("==============================")
             print(Rendez_vous.__rev_total__(Rendez_vous), "\n")
-        elif choice == 11:
-            print(main_msg)
         elif choice == 12:
+            print(main_msg)
+        elif choice == 13:
             print("A BIENTOT :)")
             exit()
 
